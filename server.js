991d15099be7d48b43122b70773dcb7b1d93173d
swisswhale <jordan.p.mclendon@gmail.com>
2025-02-19 13:35:49 -0500

    Implemented dynamic brand selection in dropdown. Used DOM manipulation to render text input when ‘Other’ is selected.

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 
 // Import watch schema
 const Watch = require("./models/Watch");
+// const watchBrands = require("./data/watchBrands") // import watch brands
 
 // Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGODB_URI);
